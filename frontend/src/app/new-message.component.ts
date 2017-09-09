78be37da7f0e2d91b8258abc0926b68f5994f4f9
HeycomRoy <cui.yao.yu@gmail.com>
2017-09-09 14:50:42 +1200

    UPDATE: message post gets user's name from the auth component.

--- frontend/src/app/new-message.component.ts
+++ frontend/src/app/new-message.component.ts
@@ -1,16 +1,17 @@
 // import { Component, Output, EventEmitter } from '@angular/core';     //******* with out the viewChild
 import { Component } from '@angular/core';
 import { WebService } from './web.service';
+import { AuthService} from './auth.service';
 
 @Component({
   selector: 'new-message',
   template: `
       <md-card class="card">
           <md-card-content>
-              <md-input-container>
-                  <!--two way bundling [(ngModel)] also need to import in app.module.ts-->
-                  <input [(ngModel)]="message.owner" mdInput placeholder="Name"/>
-              </md-input-container>
+              <!--<md-input-container>-->
+                  <!--&lt;!&ndash;two way bundling [(ngModel)] also need to import in app.module.ts&ndash;&gt;-->
+                  <!--<input [(ngModel)]="message.owner" mdInput placeholder="Name"/>-->
+              <!--</md-input-container>-->
               <md-input-container>
                   <textarea [(ngModel)]="message.text" mdInput placeholder="Message"></textarea>
               </md-input-container>
@@ -26,10 +27,10 @@ export class NewMessageComponent {
 
   // @Output() onPosted = new EventEmitter();    //******* with out the viewChild
 
-  constructor(private webService: WebService) {}
+  constructor(private webService: WebService, private auth: AuthService) {}
 
   message = {
-    owner: "",
+    owner: this.auth.name,
     text: ""
   }
 
